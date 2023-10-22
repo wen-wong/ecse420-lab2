c3cc007d3fb00428859abd6f0a91935713597b49
wen-wong <wen.wong@mail.mcgill.ca>
2023-10-22 16:37:09 -0400

    Prepare convolution file

---
+++ convolution.cu
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <gputimer.h>
+
+#include <cuda_runtime.h>
+#include <device_launch_parameters.h>
+
+__global__ void convolution(int width, int height) {
+    int index = threadIdx.x + blockIdx.x * blockDim.x;
+
+    if (index < width) {
+        // TODO - Implement convolution
+    }
+}
+
+int main(int argc, char **argv) {
+    char* input_png = argv[1];
+    char* output_png = argv[2];
+    int num_of_threads = atoi(argv[3]);
+
+    int width = 0, height = 0;
+
+    // TODO - Allocate memory for input and output images
+
+    GpuTimer timer;
+    timer.Start();
+
+    // TODO - Copy input image to device
+
+    convolution<<<1, num_of_threads>>>(width, height);
+
+    cudaDeviceSynchronize();
+
+    // TODO - Copy output image to host
+
+    timer.Stop();
+
+    printf("Time elapsed = %g ms\n", timer.Elapsed());
+
+    // TODO - Save output image
+
+    // TODO - Free memory
+}
