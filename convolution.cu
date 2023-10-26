af8f59942e1be43110d33ce8267350de10e755b0
lunamor <hong.meng@mail.mcgill.ca>
2023-10-26 12:34:39 -0400

    host code

--- convolution.cu
+++ convolution.cu
@@ -5,6 +5,8 @@
 #include <cuda_runtime.h>
 #include <device_launch_parameters.h>
 
+#include "lodepng.h"
+
 __global__ void convolution(int width, int height) {
     int index = threadIdx.x + blockIdx.x * blockDim.x;
 
@@ -18,26 +20,50 @@ int main(int argc, char **argv) {
     char* output_png = argv[2];
     int num_of_threads = atoi(argv[3]);
 
-    int width = 0, height = 0;
+    unsigned width height;
 
-    // TODO - Allocate memory for input and output images
+
+    unsigned error;
+    unsigned char *temp_image;
+    unsigned char *temp_output;
+    unsigned char *input_image;
+    unsigned char *output_image;
+
+    error = lodepng_decode32_file(&temp_image, &width, &height, input_png);
+    if (error) {
+        printf("error %u: %s", error, loadpng_error_text(error));
+    }
+
+
+    // Allocate memory for input and output images
+    cudaMalloc((void **) &input_image, width * height * 4 * sizeof(unsigned char));
+    cudaMalloc((void **) &output_image, (width - 2) * (height - 2) * 4 * sizeof(unsigned char));
+
+
+    // Copy input image to device
+    cudaMemcpy(input_image, temp_image, width * height * 4 * sizeof(unsigned char), cudaMemcpyHostToDevice);
 
     GpuTimer timer;
     timer.Start();
 
-    // TODO - Copy input image to device
-
-    convolution<<<1, num_of_threads>>>(width, height);
+    convolution<<<1, num_of_threads>>>(input_image, output_image, width, height);
 
     cudaDeviceSynchronize();
-
-    // TODO - Copy output image to host
 
     timer.Stop();
     printf("*** Time Elapsed: %g ms ***\n", timer.Elapsed());
 
-    // TODO - Save output image
+    // Copy output image to host
+    cudaMemcpy(temp_output, output_image, (width - 2) * (height - 2) * 4 * sizeof(unsigned char), cudaMemcpyDeviceToHost);
 
-    // TODO - Free memory
+    // Save output image
+    lodepng_encode32_file(output_png, temp_output, width - 2, height - 2);
+    
+    // Free memory
+    free(temp_image);
+    free(temp_output);
+    cudaFree(input_image);
+    cudaFree(output_image);
+    
     return 0;
 }
