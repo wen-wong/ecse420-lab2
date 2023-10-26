fcb603b128c43e56e9558968136fce8a00a45556
Hong Yi Meng <hong.meng@mail.mcgill.ca>
2023-10-26 13:30:40 -0400

    why doesn't this work

--- convolution.cu
+++ convolution.cu
@@ -8,10 +8,33 @@
 #include "lodepng.h"
 
 __global__ void convolution(unsigned char *input, unsigned char *output, int width, int height) {
-    int index = threadIdx.x + blockIdx.x * blockDim.x;
+    int item_count = (width - 2) * (height - 2);
 
-    if (index < width) {
-        // TODO - Implement convolution
+    int op_per_thread = item_count / (blockDim.x);
+    if (item_count % (blockDim.x)) {
+        op_per_thread++;
+        
+    }
+    printf("op per thread: %d", op_per_thread);
+
+    for(int op = 0; op < op_per_thread; op++) {
+        int index = threadIdx.x + (blockDim.x) * op;
+
+        if (index < item_count) {
+        // Implement convolution
+        printf("index: %d\n", index);
+            int i = index / (width - 2);
+            int j = index % (width - 2);
+
+            // printf("%d %d\n", i, j);
+
+            // for(int ii = 0; ii <= 2; ii++) {
+            //     for(int jj = 0; jj <= 2; jj++) {
+
+            //     }
+            // }
+
+        }   
     }
 }
 
@@ -34,6 +57,7 @@ int main(int argc, char **argv) {
         printf("error %u: %s", error, lodepng_error_text(error));
     }
 
+    temp_output = (unsigned char *) malloc((width - 2) * (height - 2) * 4 * sizeof(unsigned char));
 
     // Allocate memory for input and output images
     cudaMalloc((void **) &input_image, width * height * 4 * sizeof(unsigned char));
@@ -52,6 +76,8 @@ int main(int argc, char **argv) {
 
     timer.Stop();
     printf("*** Time Elapsed: %g ms ***\n", timer.Elapsed());
+
+    printf("error: %s\n", cudaGetErrorString(cudaGetLastError()));
 
     // Copy output image to host
     cudaMemcpy(temp_output, output_image, (width - 2) * (height - 2) * 4 * sizeof(unsigned char), cudaMemcpyDeviceToHost);
